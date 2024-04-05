5547a69c42b80eea830f01601a605bf6158c3967
Paul Lim <paulslim47@gmail.com>
2024-04-05 10:55:56 -0400

    navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   const [menu, setMenu] = useState(false);
 
-  const toggleMenu = () => {
+  const toggleNav = () => {
     setMenu((prev) => !prev);
   };
 
@@ -23,21 +23,27 @@ const Header = () => {
         <input
           type='text'
           placeholder='Search by key word or item name. . .'
+          // className={`${menu ? styles.close : undefined}`}
         />
-        {/* <img
-          className={styles.search}
-          src='search.png'
-          alt=''
-        /> */}
-        <p className={styles.headerLinks}>
-          <Link to='/login'>Sign Up</Link>
-        </p>
-        <p className={styles.headerLinks}>
-          <Link to='/signin'>Sign In</Link>
-        </p>
-        <p className={styles.headerLinks}>
-          <Link to='/cart'>Cart </Link>
-        </p>
+        <nav className={`${styles.nav_container} ${menu ? undefined : styles.close}`}>
+          <p className={styles.headerLinks}>
+            <Link to='/login'>Sign Up</Link>
+          </p>
+          <p className={styles.headerLinks}>
+            <Link to='/signin'>Sign In</Link>
+          </p>
+          <p className={styles.headerLinks}>
+            <Link to='/cart'>Cart </Link>
+          </p>
+        </nav>
+        <div
+          className={styles.hamburger}
+          onClick={toggleNav}
+        >
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+        </div>
       </div>
 
       <div className={styles.merchandise_container}>
