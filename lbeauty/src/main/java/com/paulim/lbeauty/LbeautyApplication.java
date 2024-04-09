41c8fc2ac11ab7c901909c8acbcef817dd6f8f56
Paul Lim <paulslim47@gmail.com>
2024-04-09 09:52:42 -0400

    hi

---
+++ lbeauty/src/main/java/com/paulim/lbeauty/LbeautyApplication.java
@@ -0,0 +1,12 @@
+package com.paulim.lbeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbeautyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LbeautyApplication.class, args);
+	}
+
+}
---
+++ lbeauty/src/test/java/com/paulim/lbeauty/LbeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulim.lbeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
