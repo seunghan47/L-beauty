00cc1d958eebee57d62236d1ab690c9cf13aa733
Paul Lim <paulslim47@gmail.com>
2024-10-15 19:58:13 -0400

    changing security options

--- backend/src/main/java/com/paulim/lbeauty/repository/UserRepository.java
+++ backend/src/main/java/com/paulim/lbeauty/repository/UserRepository.java
@@ -1,10 +1,10 @@
-package com.paulim.lbeauty.repository;
-
-import com.paulim.lbeauty.User.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);
-}
+//package com.paulim.lbeauty.repository;
+//
+//import com.paulim.lbeauty.User.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.Optional;
+//
+//public interface UserRepository extends JpaRepository<User, Integer> {
+//    Optional<User> findByEmail(String email);
+//}
