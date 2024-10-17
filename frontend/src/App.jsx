15ba5127cbb7b60ab5847613e6f4c5a83db9cd45
Paul Lim <paulslim47@gmail.com>
2024-10-17 16:32:45 -0400

    updated pages and api for adding to the career application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,8 @@ import { loader as itemLoader } from "./pages/ProductDetail";
 import ProductDetail from "./pages/ProductDetail";
 import ErrorPage from "./pages/ErrorPage";
 // import Products from "./pages/Products";
-import Signup from "./pages/Signup";
-import Signin from "./pages/Signin";
+// import Signup from "./pages/Signup";
+// import Signin from "./pages/Signin";
 import Home from "./pages/Home";
 import Root, { loader as headerloader } from "./pages/Root";
 import Jobs from "./pages/Jobs";
@@ -24,8 +24,8 @@ const router = createBrowserRouter([
       { index: true, element: <Home /> },
       { path: "products/:upc", element: <ProductDetail />, loader: itemLoader },
       { path: "about", element: <AboutUs /> },
-      { path: "login", element: <Signup /> },
-      { path: "signin", element: <Signin /> },
+      // { path: "login", element: <Signup /> },
+      // { path: "signin", element: <Signin /> },
       { path: "jobs", element: <Jobs /> },
       { path: "add", element: <Add /> },
       { path: "terms", element: <Terms /> },
