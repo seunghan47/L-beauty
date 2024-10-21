31dc72d0bfd602accade09c996c903fe1d86e8b1
Paul Lim <paulslim47@gmail.com>
2024-10-21 15:43:12 -0400

    getting rid of the improper authetnication process

--- backend/src/main/java/com/paulim/lbeauty/User/Role.java
+++
@@ -1,6 +0,0 @@
-package com.paulim.lbeauty.User;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- backend/src/main/java/com/paulim/lbeauty/model/Career.java
+++ backend/src/main/java/com/paulim/lbeauty/model/Career.java
@@ -1,8 +1,6 @@
 package com.paulim.lbeauty.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,22 +10,21 @@ import lombok.Setter;
 @Setter
 public class Career {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
     private String email;
     private String phone;
-    private String address;
     private String message;
 
     public Career() {
     }
 
-    public Career(String name, String email, String phone, String address, String message) {
+    public Career(String name, String email, String phone, String message) {
         this.name = name;
         this.email = email;
         this.phone = phone;
-        this.address = address;
         this.message = message;
     }
 }
