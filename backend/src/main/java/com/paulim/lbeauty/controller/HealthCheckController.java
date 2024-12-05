30c0e1eb2c6c7894eca78eda05550e673c3247e3
Paul Lim <paulslim47@gmail.com>
2024-12-05 17:20:43 -0500

    health check

--- backend/src/main/java/com/paulim/lbeauty/controller/HealthCheckController.java
+++ backend/src/main/java/com/paulim/lbeauty/controller/HealthCheckController.java
@@ -1,2 +1,16 @@
-package com.paulim.lbeauty.controller;public class HealthCheckController {
+package com.paulim.lbeauty.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("ok");
+    }
 }
