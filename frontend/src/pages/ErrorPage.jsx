9e888e5d54c4ba65a16225290bdb7caba4066669
Paul Lim <paulslim47@gmail.com>
2024-12-05 16:16:34 -0500

    error page revision

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -3,13 +3,48 @@ import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Oops, looks like this page hasn't been developed yet</h1>
-      <h1>
-        click <Link to='..'>this</Link> to go back
-      </h1>
+    <div style={styles.container}>
+      <h1 style={styles.title}>Oops! We're doing some maintenance.</h1>
+      <p style={styles.message}>We're currently making improvements to this page. Please check back in a few hours.</p>
+      <Link
+        to='..'
+        style={styles.link}
+      >
+        Go Back to Safety
+      </Link>
     </div>
   );
+};
+
+const styles = {
+  container: {
+    textAlign: "center",
+    padding: "2rem",
+    fontFamily: "Arial, sans-serif",
+    color: "#333",
+    backgroundColor: "#f8f8f8",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: "2rem",
+    marginBottom: "1rem",
+  },
+  message: {
+    fontSize: "1.2rem",
+    marginBottom: "2rem",
+  },
+  link: {
+    textDecoration: "none",
+    padding: "0.5rem 1rem",
+    backgroundColor: "#007BFF",
+    color: "#fff",
+    borderRadius: "5px",
+    fontSize: "1rem",
+  },
 };
 
 export default ErrorPage;
