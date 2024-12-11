52a82e633f7cbf226279f471953440f80a5733ef
Paul Lim <paulslim47@gmail.com>
2024-12-10 20:48:14 -0500

    added swagger for easier visual representation

--- backend/src/main/java/com/paulim/lbeauty/LbeautyApplication.java
+++ backend/src/main/java/com/paulim/lbeauty/LbeautyApplication.java
@@ -1,6 +1,5 @@
 package com.paulim.lbeauty;
 
-import com.paulim.lbeauty.service.NewItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
