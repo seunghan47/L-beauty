f458c5a5daa1df0cfddd36ec1b73cc2a773fc523
Paul Lim <paulslim47@gmail.com>
2024-12-11 10:43:49 -0500

    adding swagger for api documentation

---
+++ backend/src/main/java/com/paulim/lbeauty/config/SwaggerConfig.java
@@ -0,0 +1,8 @@
+package com.paulim.lbeauty.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    //
+}
--- backend/src/main/java/com/paulim/lbeauty/controller/HealthCheckController.java
+++ backend/src/main/java/com/paulim/lbeauty/controller/HealthCheckController.java
@@ -1,5 +1,8 @@
 package com.paulim.lbeauty.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
 
     @GetMapping
+    @Operation(
+            summary = "Health Check",
+            description = "Checks and returns a message to see if APIs are working"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "API is healthy"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("api is up and running");
     }
