6635a062d6be8bf04611f53a87cab2fd1a27372e
Paul Lim <paulslim47@gmail.com>
2025-01-08 14:21:23 -0500

    attempting to add dynamic brand changes to sidebars

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,30 +1,36 @@
-import React from "react";
 import styles from "./Sidebar.module.css";
 
-const Sidebar = ({ category }) => {
+const Sidebar = ({ category, brand }) => {
   return (
     <div className={styles.sidebar}>
-      <h3>Refine</h3>
       <div>
-        <h4>Type</h4>
-        <p>• {category.replace("-", " ")}</p>
+        <h1>Filter By</h1>
       </div>
       <div>
-        <h4>Brand</h4>
+        <h2>Brand</h2>
         <ul>
-          <li>ACME East</li>
-          <li>Coaster Z2 Premium</li>
-          <li>Ashley Furniture</li>
+          {brand.map((brand, index) => (
+            <li key={index}> {brand}</li>
+          ))}
         </ul>
       </div>
       <div>
-        <h4>Price</h4>
+        <h2>Price</h2>
         <ul>
-          <li>Under $500</li>
-          <li>$500 to $1,000</li>
-          <li>$1,000 to $1,500</li>
-          <li>$1,500 to $2,000</li>
-          <li>Over $2,500</li>
+          <li>Under $10</li>
+          <li>$10 to $30</li>
+          <li>$30 to $50</li>
+          <li>$50 to $70</li>
+          <li>Over $70</li>
+        </ul>
+      </div>
+      <div>
+        <h2>Customer Rating</h2>
+        <ul>
+          <li>5 Stars</li>
+          <li>4 Stars</li>
+          <li>3 Stars</li>
+          <li>2 Stars</li>
         </ul>
       </div>
     </div>
