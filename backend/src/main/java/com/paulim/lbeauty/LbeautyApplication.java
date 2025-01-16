099658fe84b069145ca559c80189dd99f4c17d38
Paul Lim <paulslim47@gmail.com>
2025-01-15 19:58:15 -0500

    added test cases

--- backend/src/main/java/com/paulim/lbeauty/LbeautyApplication.java
+++ backend/src/main/java/com/paulim/lbeauty/LbeautyApplication.java
@@ -1,10 +1,7 @@
 package com.paulim.lbeauty;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class LbeautyApplication {
--- backend/src/test/java/com/paulim/lbeauty/service/ClickedItemsServiceTest.java
+++ backend/src/test/java/com/paulim/lbeauty/service/ClickedItemsServiceTest.java
@@ -1,4 +1,32 @@
 package com.paulim.lbeauty.service;
 
+import com.paulim.lbeauty.model.ClickedItems;
+import com.paulim.lbeauty.repository.ClickedItemsRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class ClickedItemsServiceTest {
+
+    @Mock
+    ClickedItemsRepository clickedItemsRepository;
+
+    @InjectMocks
+    ClickedItemsService clickedItemsService;
+
+    @Test
+    void savedItemSuccess() {
+        LocalDate today = LocalDate.now();
+        ClickedItems item = new ClickedItems("item1", "123456", today);
+        clickedItemsService.save(item);
+        verify(clickedItemsRepository).save(item);
+
+    }
 }
