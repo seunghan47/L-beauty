6afebd2b126e2d0325ac3b0d83fba9df0f463ae0
Paul Lim <paulslim47@gmail.com>
2025-01-22 16:15:32 -0500

    adding gitfiles for python scripts

---
+++ aws/ec2/lambda_function_stop.py
@@ -0,0 +1,20 @@
+import boto3
+
+
+def lambda_handler(event, context):
+    instance_id = "i-05cc78120cdcb8d6e"
+    region = "us-east-1"
+
+    try:
+        ec2 = boto3.client('ec2', region_name=region)
+        response = ec2.stop_instances(InstanceIds=[instance_id])
+        return {
+            "statusCode": 200,
+            "body": f"Instance stopped: {instance_id}",
+            "response": response
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "error": str(e)
+        }
