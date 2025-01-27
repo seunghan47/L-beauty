952f051893430e2ebacca3f9f38f9af3c8d1173a
Paul Lim <paulslim47@gmail.com>
2025-01-27 12:06:41 -0500

    connections to sql sever

---
+++ aws/s3/s3_utils.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+
+def get_missing_image():
+    try:
+        conn = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME
+        )
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+
+    
+    cursor = conn.cursor()
+    query = "SELECT * FROM inventory LIMIT 100;"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return results
+   
+if __name__ == "__main__":
+    results = get_missing_image()
+    for row in results:
+        print(row)
