22f82f080584a976aa1192dc2c18b3a0327f248b
Paul Lim <paulslim47@gmail.com>
2026-03-30 20:05:02 -0400

    api descriptions

--- backend/src/main/java/com/paulim/lbeauty/career/CareerController.java
+++ backend/src/main/java/com/paulim/lbeauty/career/CareerController.java
@@ -1,5 +1,7 @@
 package com.paulim.lbeauty.career;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,27 +12,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/career")
 @CrossOrigin("*")
+@Tag(name = "Career Management", description = "Endpoints for handling job submissions and inquiries")
 public class CareerController {
 
     @Autowired
-    CareerService careerService;
+    private CareerService careerService;
 
+    @Operation(summary = "Submit a job application", description = "Saves user contact info and a message to the database for hiring review.")
     @PostMapping("/addCareer")
     public ResponseEntity<Career> addJob(@RequestBody Career career) {
-
-        System.out.println("Received Career data: " + career);
-        System.out.println("email : " + career.getEmail());
-        System.out.println("message : " + career.getMessage());
-        System.out.println("name : " + career.getName());
-        System.out.println("phone : " + career.getPhone());
-
-        Career saved = careerService.save(career);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+        System.out.println("Career object received: " + career);
+        Career savedCareer = careerService.save(career);
+        return new ResponseEntity<>(savedCareer, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get all applications", description = "Retrieves a full list of all submitted career inquiries from the database.")
     @GetMapping("/getAll")
     public ResponseEntity<List<Career>> getAll() {
-        return ResponseEntity.ok(careerService.getAll());
+        List<Career> careerList = careerService.getAll();
+        return new ResponseEntity<>(careerList, HttpStatus.OK);
     }
 }
