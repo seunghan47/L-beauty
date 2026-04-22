3ec711b94a4c16d781f50ed406cf708ae87b5a45
Paul Lim <paulslim47@gmail.com>
2026-04-21 22:57:25 -0400

    add: lombok for simplification

--- backend/src/main/java/com/paulim/lbeauty/career/Career.java
+++ backend/src/main/java/com/paulim/lbeauty/career/Career.java
@@ -1,60 +1,31 @@
 package com.paulim.lbeauty.career;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table (name = "career")
-
+@Table(name = "career")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Career {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Phone number is required")
     private String phone;
+
     private String message;
-
-    public Career() {
-    }
-
-    public Career(String name, String email, String phone, String message) {
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.message = message;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
-
