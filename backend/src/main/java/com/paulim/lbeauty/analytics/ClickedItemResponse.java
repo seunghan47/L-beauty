59e44975e52fcae1b11808740466daf8537ce840
Paul Lim <paulslim47@gmail.com>
2026-04-28 19:12:24 -0400

    add: DTO response

---
+++ backend/src/main/java/com/paulim/lbeauty/analytics/ClickedItemResponse.java
@@ -0,0 +1,12 @@
+package com.paulim.lbeauty.analytics;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ClickedItemResponse {
+    private long id;
+    private String name;
+    private String upc;
+    private LocalDateTime createdAt;
+    private LocalDate date;
+}
