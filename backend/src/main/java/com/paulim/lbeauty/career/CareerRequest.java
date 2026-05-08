e9a99dc856d0105423567659532b3016eb44ac23
Paul Lim <paulslim47@gmail.com>
2026-05-07 22:55:23 -0400

    add: dto for career

---
+++ backend/src/main/java/com/paulim/lbeauty/career/CareerRequest.java
@@ -0,0 +1,8 @@
+package com.paulim.lbeauty.career;
+
+public class CareerRequest {
+    private String name;
+    private String email;
+    private String phone;
+    private String message;
+}
---
+++ backend/src/main/java/com/paulim/lbeauty/career/CareerResponse.java
@@ -0,0 +1,9 @@
+package com.paulim.lbeauty.career;
+
+public class CareerResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String message;
+}
